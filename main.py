b135645ca68b7955b74b904fb5835b232635ca2b
Kévin Dérécusson <derec84@hotmail.fr>
2021-12-06 16:08:30 +0100

    Create csv file with a list of products
    extract_product_data_in_csv() now use a list of URL
    pointing to products pages to create a csv file
    with data for each products page of the list.
    Now, 'title' is formatted to correspond
    to the standard of Windows files name.

--- main.py
+++ main.py
@@ -6,39 +6,42 @@ from bs4 import BeautifulSoup
 category_url = 'http://books.toscrape.com/catalogue/category/books/default_15/index.html'
 product_url_list = []
 
-def extract_product_data_in_csv():
-    url = 'http://books.toscrape.com/catalogue/soumission_998/index.html'
-    page = requests.get(url).text
-    soup = BeautifulSoup(page, 'html.parser')
 
-    product_page_url = url
-    universal_product_code = soup.find_all('td')[0].text
-    title = soup.h1.text
-    price_including_tax = soup.find_all('td')[3].text.replace('Â', '').replace('£','')
-    price_excluding_tax = soup.find_all('td')[2].text.replace('Â', '').replace('£','')
-    number_available = soup.find_all('td')[-2].text
-    number_available = ''.join(i for i in number_available if i.isdigit())
-    product_description = soup.h2.next_sibling.next_element.next_element.text
-    category = soup.find_all('a')[3].text
-    review_rating = soup.find(class_='star-rating')
-    review_rating = review_rating.get('class')[1]
-    image_url = soup.img['src']
-    image_url = 'http://books.toscrape.com/' + image_url
-
-    #Création du fichier csv
+def extract_product_data_in_csv(self):
     os.chdir(os.getcwd() + '\data\csv')
-    columns_name = ('product_page_url', 'universal_product_code', 'title', 
-                    'price_including_tax', 'price_excluding_tax', 
-                    'number_available', 'product_description', 'category', 
-                    'review_rating', 'image_url')
-    data = [product_page_url, universal_product_code, title, 
-            price_including_tax, price_excluding_tax, number_available, 
-            product_description, category, review_rating, image_url]
-    filename = '{}.csv'.format(title)
-    with open(filename, 'w', encoding="utf-8", newline='') as new:
-        writer = csv.writer(new)
-        writer.writerow(columns_name)
-        writer.writerow(data)
+    for url in self:
+        page = requests.get(url).content
+        soup = BeautifulSoup(page, 'html.parser')
+
+        product_page_url = url
+        universal_product_code = soup.find_all('td')[0].text
+        title = soup.h1.text.replace("<","").replace(">","").replace(":"," ")
+        title = title.replace("«","").replace("»","").replace("|","")
+        title = title.replace("?","").replace("*","").replace("."," ")
+        title = title.replace('"',"").replace('/'," ").rstrip()
+        price_including_tax = soup.find_all('td')[3].text.replace('£','')
+        price_excluding_tax = soup.find_all('td')[2].text.replace('£','')
+        number_available = soup.find_all('td')[-2].text
+        number_available = ''.join(i for i in number_available if i.isdigit())
+        product_description = soup.h2.next_sibling.next_element.next_element.text
+        category = soup.find_all('a')[3].text
+        review_rating = soup.find(class_='star-rating')
+        review_rating = review_rating.get('class')[1]
+        image_url = 'http://books.toscrape.com/' + soup.img['src'][6:]
+
+        #Création du fichier csv
+        columns_name = ('product_page_url', 'universal_product_code', 'title', 
+                        'price_including_tax', 'price_excluding_tax', 
+                        'number_available', 'product_description', 'category', 
+                        'review_rating', 'image_url')
+        data = [product_page_url, universal_product_code, title, 
+                price_including_tax, price_excluding_tax, number_available, 
+                product_description, category, review_rating, image_url]
+        filename = '{}.csv'.format(title)
+        with open(filename, 'w', encoding="utf-8", newline='') as new:
+            writer = csv.writer(new)
+            writer.writerow(columns_name)
+            writer.writerow(data)
 
 def check_next_page(self):
     page = requests.get(self).content
