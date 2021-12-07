0bff2e45a523c09ab5a3dcc7ceab660837eaccb9
Kévin Dérécusson <derec84@hotmail.fr>
2021-12-07 13:47:56 +0100

    Create csv files organize by category name
    Now, csv file are named by category and
    contains all the products data of the category.
    'title' no longer need to respect Windows file name standard.
    New function create_new_csv() to separate
    creation and modification of csv.

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-category_url = 'http://books.toscrape.com/catalogue/category/books/default_15/index.html'
-product_url_list = []
+os.chdir(os.getcwd() + '\data\csv')
 
 home_page = 'https://books.toscrape.com/index.html'
 
+
+def create_new_csv(self):
+    page = requests.get(self).content
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    category = soup.find_all('a')[3].text
+    columns_name = ('product_page_url', 'universal_product_code', 'title', 
+                    'price_including_tax', 'price_excluding_tax', 
+                    'number_available', 'product_description', 'category', 
+                    'review_rating', 'image_url')
+    filename = '{}.csv'.format(category)
+
+    with open(filename, 'w', encoding="utf-8", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(columns_name)
+
+    return category
+
 def extract_product_data_in_csv(self):
-    os.chdir(os.getcwd() + '\data\csv')
     for url in self:
         page = requests.get(url).content
         soup = BeautifulSoup(page, 'html.parser')
 
         product_page_url = url
         universal_product_code = soup.find_all('td')[0].text
-        title = soup.h1.text.replace("<","").replace(">","").replace(":"," ")
-        title = title.replace("«","").replace("»","").replace("|","")
-        title = title.replace("?","").replace("*","").replace("."," ")
-        title = title.replace('"',"").replace('/'," ").rstrip()
+        title = soup.h1.text
         price_including_tax = soup.find_all('td')[3].text.replace('£','')
         price_excluding_tax = soup.find_all('td')[2].text.replace('£','')
         number_available = soup.find_all('td')[-2].text
@@ -30,18 +43,13 @@ def extract_product_data_in_csv(self):
         review_rating = review_rating.get('class')[1]
         image_url = 'http://books.toscrape.com/' + soup.img['src'][6:]
 
-        #Création du fichier csv
-        columns_name = ('product_page_url', 'universal_product_code', 'title', 
-                        'price_including_tax', 'price_excluding_tax', 
-                        'number_available', 'product_description', 'category', 
-                        'review_rating', 'image_url')
         data = [product_page_url, universal_product_code, title, 
-                price_including_tax, price_excluding_tax, number_available, 
-                product_description, category, review_rating, image_url]
-        filename = '{}.csv'.format(title)
-        with open(filename, 'w', encoding="utf-8", newline='') as new:
-            writer = csv.writer(new)
-            writer.writerow(columns_name)
+        price_including_tax, price_excluding_tax, number_available, 
+        product_description, category, review_rating, image_url]
+
+        filename = '{}.csv'.format(category)
+        with open(filename, 'a', encoding="utf-8", newline='') as f:
+            writer = csv.writer(f)
             writer.writerow(data)
 
 def check_next_page(self):
@@ -88,5 +96,9 @@ def create_category_url_list(self):
 category_url_list = []
 category_url_list = create_category_url_list(home_page)
 
-product_url_list = create_product_url_list_of_a_category(category_url)
-extract_product_data_in_csv(product_url_list)
+for url in category_url_list:
+    product_url_list = []
+    product_url_list = create_product_url_list_of_a_category(url)
+    category = create_new_csv(product_url_list[0])
+    extract_product_data_in_csv(product_url_list)
+    print('Traitement de la catégorie "{}" terminée.'.format(category))
